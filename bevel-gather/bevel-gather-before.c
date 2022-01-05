57b7eae46759b14b677fd56fc3ab79f66a4329dd
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-01-05 10:08:50 +0100

    gather hostname

--- bevel-gather/bevel-gather-before.c
+++ bevel-gather/bevel-gather-before.c
@@ -1,10 +1,12 @@
 #include <inttypes.h>
+#include <limits.h>
 #include <sqlite3.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 #define DB_FILE "/home/syd/.local/share/bevel/data.sqlite3"
 
@@ -26,10 +28,7 @@ int main(void) {
   int opened = sqlite3_open(DB_FILE, &db);
 
   if (opened != SQLITE_OK) {
-    fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
-    sqlite3_close(db);
-
-    return 1;
+    goto error;
   }
 
   sqlite3_stmt *stmt;
@@ -40,43 +39,46 @@ int main(void) {
                          -1, &stmt, NULL);
 
   if (prepared != SQLITE_OK) {
-    fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
-    sqlite3_close(db);
-
-    return 1;
+    goto error;
   }
 
   // We need these things for a "command has started" insertion:
   //
   // 1. Text of the command
+
   // 2. Begin time
+  sqlite3_int64 begin = getTimeMillis();
   // 3. Workdir
   // 4. User
   // 5. Host
+  char hostname[HOST_NAME_MAX + 1];
+  gethostname(hostname, HOST_NAME_MAX + 1);
 
+  // We bind all these values to the prepared statement for insertion
   sqlite3_bind_text(stmt, 1, "here goes the command", -1, NULL);
-  sqlite3_bind_int64(stmt, 2, getTimeMillis());
+  sqlite3_bind_int64(stmt, 2, begin);
   sqlite3_bind_text(stmt, 3, "here goes the workdir", -1, NULL);
   sqlite3_bind_text(stmt, 4, "here goes the user", -1, NULL);
-  sqlite3_bind_text(stmt, 5, "here goes the host", -1, NULL);
+  sqlite3_bind_text(stmt, 5, hostname, -1, NULL);
 
   // TODO deal with errors here
   int inserted = sqlite3_step(stmt);
 
   if (inserted != SQLITE_DONE) {
-    fprintf(stderr, "Failed to insert command: %s\n", sqlite3_errmsg(db));
-    sqlite3_close(db);
-
-    return 1;
+    goto error;
   }
 
   sqlite3_int64 command_id = sqlite3_last_insert_rowid(db);
   printf("%lld\n", command_id);
 
-
   sqlite3_finalize(stmt);
   sqlite3_close(db);
 
   return 0;
-}
 
+error:
+  fprintf(stderr, "Failed to insert command: %s\n", sqlite3_errmsg(db));
+  sqlite3_close(db);
+
+  return 1;
+}
