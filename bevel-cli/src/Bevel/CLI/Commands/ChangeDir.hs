8cc74d83d058f916acf4cf247341852600d9456e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-01-06 13:05:45 +0100

    very basic directory switching cli

--- bevel-cli/src/Bevel/CLI/Commands/ChangeDir.hs
+++ bevel-cli/src/Bevel/CLI/Commands/ChangeDir.hs
@@ -16,10 +16,16 @@ import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Reader
 import Cursor.Brick.List.NonEmpty
+import Cursor.Brick.Text
 import Cursor.Simple.List.NonEmpty
+import Cursor.Text
+import Cursor.Types
 import qualified Data.List.NonEmpty as NE
 import Data.Maybe
+import Data.Set (Set)
 import qualified Data.Set as S
+import Data.Text (Text)
+import qualified Data.Text as T
 import Database.Persist
 import Graphics.Vty (defaultConfig, mkVty, outputFd)
 import Graphics.Vty.Attributes
@@ -40,7 +46,7 @@ changeDir = do
     let runWorker = runReaderT (tuiWorker reqChan respChan) workerEnv
     -- Left always works because the worker runs forever
     Left endState <- race runTui runWorker
-    forM_ (stateDirs endState) $ \nec ->
+    forM_ (stateOptions endState) $ \nec ->
       putStrLn $ fromAbsDir $ nonEmptyCursorCurrent nec
 
 tuiApp :: BChan Request -> App State Response ResourceName
@@ -54,20 +60,21 @@ tuiApp chan =
     }
 
 data State = State
-  { stateDirs :: Maybe (NonEmptyCursor (Path Abs Dir))
+  { stateDirs :: Set (Path Abs Dir),
+    stateOptions :: !(Maybe (NonEmptyCursor (Path Abs Dir))),
+    stateSearch :: !TextCursor
   }
   deriving (Show)
 
-data ResourceName = ResourceName
+data ResourceName = SearchBox
   deriving (Show, Eq, Ord)
 
 buildInitialState :: C State
 buildInitialState = do
-  dirs <-
-    runDB $
-      S.toList . S.fromList . map (\(Entity _ ClientCommand {..}) -> clientCommandWorkdir)
-        <$> selectList [] []
-  let stateDirs = makeNonEmptyCursor <$> NE.nonEmpty dirs
+  dirs <- runDB $ S.fromList . map (\(Entity _ ClientCommand {..}) -> clientCommandWorkdir) <$> selectList [] []
+  let stateDirs = dirs
+  let stateOptions = makeNonEmptyCursor <$> NE.nonEmpty (S.toList dirs)
+  let stateSearch = emptyTextCursor
   pure State {..}
 
 buildAttrMap :: State -> AttrMap
@@ -83,29 +90,53 @@ selectedAttr = "selected"
 
 drawTui :: State -> [Widget ResourceName]
 drawTui State {..} =
-  [ case stateDirs of
-      Nothing -> str "Empty"
-      Just dirs ->
-        let goDir = str . fromAbsDir
-         in verticalNonEmptyCursorWidget
-              goDir
-              (withAttr selectedAttr . goDir)
-              goDir
-              dirs
+  [ vBox
+      [ padTop Max $ case stateOptions of
+          Nothing -> str "Empty"
+          Just dirs ->
+            let goDir = str . fromAbsDir
+             in nonEmptyCursorWidget
+                  ( \befores current afters ->
+                      vBox $
+                        reverse $
+                          concat [map goDir befores, [withAttr selectedAttr $ goDir current], map goDir afters]
+                  )
+                  dirs,
+        visible $ withAttr selectedAttr $ selectedTextCursorWidget SearchBox stateSearch
+      ]
   ]
+
+fuzzySearch :: Text -> Path Abs Dir -> Bool
+fuzzySearch query path = null $ T.foldl' go (T.unpack (T.toCaseFold query)) (T.toCaseFold (T.pack (fromAbsDir path)))
+  where
+    go :: [Char] -> Char -> [Char]
+    go [] _ = []
+    go q@(qc : rest) dc
+      | qc == dc = rest
+      | otherwise = q
 
 handleTuiEvent :: BChan Request -> State -> BrickEvent n Response -> EventM n (Next State)
 handleTuiEvent _ s e =
   case e of
     VtyEvent vtye ->
-      let modList func = continue $ s {stateDirs = func <$> stateDirs s}
-          modMList mFunc = modList $ \nec -> fromMaybe nec $ mFunc nec
+      let modOptions func = continue $ s {stateOptions = func <$> stateOptions s}
+          modMOptions mFunc = modOptions $ \nec -> fromMaybe nec $ mFunc nec
+          modSearch func = do
+            let newSearch = func $ stateSearch s
+            let query = rebuildTextCursor newSearch
+            let newOptions = S.toList $ S.filter (fuzzySearch query) (stateDirs s)
+            let newOptionsCursor = makeNonEmptyCursor <$> NE.nonEmpty newOptions
+            continue $ s {stateSearch = newSearch, stateOptions = newOptionsCursor}
+          modMSearch mFunc = modSearch $ \tc -> fromMaybe tc $ mFunc tc
        in case vtye of
             EvKey KEnter [] -> halt s
-            EvKey KDown [] -> modMList nonEmptyCursorSelectNext
-            EvKey KUp [] -> modMList nonEmptyCursorSelectPrev
-            EvKey KLeft [] -> modList nonEmptyCursorSelectFirst
-            EvKey KRight [] -> modList nonEmptyCursorSelectLast
+            EvKey KDown [] -> modMOptions nonEmptyCursorSelectPrev
+            EvKey KUp [] -> modMOptions nonEmptyCursorSelectNext
+            EvKey (KChar c) [] -> modMSearch $ textCursorInsert c
+            EvKey KBS [] -> modMSearch $ dullMDelete . textCursorRemove
+            EvKey KDel [] -> modMSearch $ dullMDelete . textCursorDelete
+            EvKey KLeft [] -> modMSearch textCursorSelectPrev
+            EvKey KRight [] -> modMSearch textCursorSelectNext
             _ -> continue s
     AppEvent resp -> case resp of
       Response -> continue s
