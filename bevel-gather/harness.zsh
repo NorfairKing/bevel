d50fea6bbe2206387976ff89870e38e98b2cf9e8
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-01-07 11:07:08 +0100

    Working cd harness for zsh

--- bevel-gather/harness.zsh
+++ bevel-gather/harness.zsh
@@ -22,3 +22,17 @@ _bevel_precmd(){
 
 add-zsh-hook preexec _bevel_preexec
 add-zsh-hook precmd _bevel_precmd
+
+function _bevel_cd {
+  zle -I # Invalidate current display
+  
+  # The stdin is closed in zle widgets (for whatever reason) so we have to read
+  # directly from the tty instead of from stdin.
+  # https://stackoverflow.com/questions/57539180/why-is-interactive-command-breaking-when-using-zsh-widget-to-execute-it
+  cd $(bevel cd </dev/tty)
+
+	zle reset-prompt # Re-expand prompt
+}
+zle -N _bevel_cd_widget _bevel_cd
+
+bindkey '^p' _bevel_cd_widget
