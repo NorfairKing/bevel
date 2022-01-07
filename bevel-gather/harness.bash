6cb70da46fa65f2f923fb2f3792d3ea6e48667b9
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-01-07 18:45:19 +0100

    command binding for bevel repeat for bash and zsh

--- bevel-gather/harness.bash
+++ bevel-gather/harness.bash
@@ -29,3 +29,16 @@ _bevel_cd() {
 }
 
 bind -x '"\C-p": _bevel_cd'
+
+_bevel_repeat ()
+{
+	tput rmkx
+  command="$(bevel repeat)"
+	tput smkx
+
+  READLINE_LINE=${command}
+  READLINE_POINT=${#READLINE_LINE}
+}
+
+bind -x '"\C-r": _bevel_repeat'
+
--- bevel-gather/harness.zsh
+++ bevel-gather/harness.zsh
@@ -23,12 +23,15 @@ _bevel_precmd(){
 add-zsh-hook preexec _bevel_preexec
 add-zsh-hook precmd _bevel_precmd
 
+# [Note: stdin zle widgets]
+# The stdin is closed in zle widgets (for whatever reason) so we have to read
+# directly from the tty instead of from stdin.
+# https://stackoverflow.com/questions/57539180/why-is-interactive-command-breaking-when-using-zsh-widget-to-execute-it
+
 function _bevel_cd {
   zle -I # Invalidate current display
   
-  # The stdin is closed in zle widgets (for whatever reason) so we have to read
-  # directly from the tty instead of from stdin.
-  # https://stackoverflow.com/questions/57539180/why-is-interactive-command-breaking-when-using-zsh-widget-to-execute-it
+  # See [Note: stdin zle widgets]
   cd $(bevel cd </dev/tty)
 
 	zle reset-prompt # Re-expand prompt
@@ -36,3 +39,23 @@ function _bevel_cd {
 zle -N _bevel_cd_widget _bevel_cd
 
 bindkey '^p' _bevel_cd_widget
+
+function _bevel_repeat {
+  zle -I # Invalidate current display
+
+  # See [Note: stdin zle widgets]
+  command=$(bevel repeat </dev/tty)
+
+	if [[ -n $command ]] ; then
+		LBUFFER=$command
+	fi
+
+	zle reset-prompt
+}
+
+zle -N _bevel_repeat_widget _bevel_repeat
+bindkey '^r' _bevel_repeat_widget
+bindkey '^[[A' _bevel_repeat_widget
+bindkey '^[OA' _bevel_repeat_widget
+
+
