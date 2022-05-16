b0631ebeeb69263a8ad43a2219f6d11623111f79
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-05-17 00:37:12 +0200

    Build the shell on CI as well

--- ci.nix
+++ ci.nix
@@ -1,13 +1,17 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import ./nix/pkgs.nix { inherit sources; };
-  pre-commit-hooks = import ./nix/pre-commit.nix { inherit sources; };
+  pre-commit = import ./nix/pre-commit.nix { inherit sources; };
 in
 {
   "release" = pkgs.bevelRelease;
-  "pre-commit-hooks" = pre-commit-hooks.run;
+  "pre-commit-check" = pre-commit.run;
   "nixos-module-test" = import ./nix/nixos-module-test.nix {
     inherit sources;
     pkgs = pkgs;
+  };
+  "shell" = pkgs.symlinkJoin {
+    name = "shell";
+    paths = (import ./shell.nix { inherit sources pkgs pre-commit; }).buildInputs;
   };
 }
