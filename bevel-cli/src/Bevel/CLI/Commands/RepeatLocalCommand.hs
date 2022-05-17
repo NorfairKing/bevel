9811b93cf3de6414d4090be696734d2c1238d1be
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-05-17 10:40:51 +0200

    refactor RepeatLocalCommand

--- bevel-cli/src/Bevel/CLI/Commands/RepeatLocalCommand.hs
+++ bevel-cli/src/Bevel/CLI/Commands/RepeatLocalCommand.hs
@@ -5,26 +5,35 @@ module Bevel.CLI.Commands.RepeatLocalCommand (repeatLocalCommand) where
 import Bevel.CLI.Env
 import Bevel.CLI.Select
 import Bevel.Client.Data
+import Data.Word
 import Database.Esqueleto.Experimental
+import Path
 import Path.IO
 
 repeatLocalCommand :: C ()
 repeatLocalCommand = do
   here <- getCurrentDir
-  let whereClauses clientCommand =
-        where_ $ clientCommand ^. ClientCommandWorkdir ==. val here
-
   selectApp
     SelectAppSettings
-      { selectAppSettingCount = fmap (maybe 0 unValue) $
-          selectOne $ do
-            clientCommand <- from $ table @ClientCommand
-            whereClauses clientCommand
-            pure countRows,
-        selectAppSettingLoadSource = selectSource $ do
-          clientCommand <- from $ table @ClientCommand
-          whereClauses clientCommand
-          -- New to old, because newer ones are the most important according to our formula
-          orderBy [desc $ clientCommand ^. ClientCommandBegin]
-          pure (clientCommand ^. ClientCommandBegin, clientCommand ^. ClientCommandText)
+      { selectAppSettingCount = repeatLocalCommandCount here,
+        selectAppSettingLoadSource = repeatLocalCommandLoadSource here
       }
+
+repeatLocalCommandCount :: Path Abs Dir -> SqlPersistT IO Word64
+repeatLocalCommandCount here = fmap (maybe 0 unValue) $
+  selectOne $ do
+    clientCommand <- from $ table @ClientCommand
+    repeatLocalWhereClauses here clientCommand
+    pure countRows
+
+repeatLocalCommandLoadSource :: Path Abs Dir -> LoadSource
+repeatLocalCommandLoadSource here = selectSource $ do
+  clientCommand <- from $ table @ClientCommand
+  repeatLocalWhereClauses here clientCommand
+  -- New to old, because newer ones are the most important according to our formula
+  orderBy [desc $ clientCommand ^. ClientCommandBegin]
+  pure (clientCommand ^. ClientCommandBegin, clientCommand ^. ClientCommandText)
+
+repeatLocalWhereClauses :: Path Abs Dir -> SqlExpr (Entity ClientCommand) -> SqlQuery ()
+repeatLocalWhereClauses here clientCommand =
+  where_ $ clientCommand ^. ClientCommandWorkdir ==. val here
