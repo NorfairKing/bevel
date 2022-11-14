165b54670da9099733f22c70f17da94f9fa9a17e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-11-14 16:26:02 +0100

    get rid of flake-utils

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-22.05";
     home-manager.url = "github:nix-community/home-manager?ref=release-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     validity.url = "github:NorfairKing/validity?ref=flake";
     validity.flake = false;
@@ -25,7 +24,6 @@
     { self
     , nixpkgs
     , home-manager
-    , flake-utils
     , pre-commit-hooks
     , validity
     , safe-coloured-text
@@ -33,70 +31,70 @@
     , autodocodec
     , appendful
     }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ]
-      (system:
-      let
-        pkgsFor = nixpkgs: import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-          overlays = [
-            self.overlays.${system}
-            (import (autodocodec + "/nix/overlay.nix"))
-            (import (safe-coloured-text + "/nix/overlay.nix"))
-            (import (sydtest + "/nix/overlay.nix"))
-            (import (appendful + "/nix/overlay.nix"))
-            (import (validity + "/nix/overlay.nix"))
-          ];
+    let
+      system = "x86_64-linux";
+      pkgsFor = nixpkgs: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+          self.overlays.${system}
+          (import (autodocodec + "/nix/overlay.nix"))
+          (import (safe-coloured-text + "/nix/overlay.nix"))
+          (import (sydtest + "/nix/overlay.nix"))
+          (import (appendful + "/nix/overlay.nix"))
+          (import (validity + "/nix/overlay.nix"))
+        ];
+      };
+      pkgs = pkgsFor nixpkgs;
+      mkNixosModule = import ./nix/nixos-module.nix { inherit (pkgs.bevelReleasePackages) bevel-api-server; };
+    in
+    {
+      overlays.${system} = import ./nix/overlay.nix;
+      packages.${system}.default = pkgs.bevelRelease;
+      checks.${system} = {
+        release = self.packages.${system}.default;
+        shell = self.devShells.${system}.default;
+        nixos-module-test = import ./nix/nixos-module-test.nix {
+          inherit pkgs;
+          home-manager = home-manager.nixosModules.home-manager;
+          bevel-nixos-module-factory = self.nixosModuleFactories.${system}.default;
+          bevel-home-manager-module = self.homeManagerModules.${system}.default;
         };
-        pkgs = pkgsFor nixpkgs;
-        mkNixosModule = import ./nix/nixos-module.nix { inherit (pkgs.bevelReleasePackages) bevel-api-server; };
-      in
-      {
-        overlays = import ./nix/overlay.nix;
-        packages.release = pkgs.bevelRelease;
-        packages.default = self.packages.${system}.release;
-        checks = {
-          nixos-module-test = import ./nix/nixos-module-test.nix {
-            inherit pkgs;
-            home-manager = home-manager.nixosModules.home-manager;
-            bevel-nixos-module-factory = self.nixosModuleFactories.${system}.default;
-            bevel-home-manager-module = self.homeManagerModules.${system}.default;
+        pre-commit = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            hlint.enable = true;
+            hpack.enable = true;
+            ormolu.enable = true;
+            nixpkgs-fmt.enable = true;
+            nixpkgs-fmt.excludes = [ ".*/default.nix" ];
+            cabal2nix.enable = true;
           };
-          pre-commit = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              hlint.enable = true;
-              hpack.enable = true;
-              ormolu.enable = true;
-              nixpkgs-fmt.enable = true;
-              nixpkgs-fmt.excludes = [ ".*/default.nix" ];
-              cabal2nix.enable = true;
-            };
-          };
         };
-        devShells.default = pkgs.haskellPackages.shellFor {
-          name = "bevel-shell";
-          packages = (p:
-            (builtins.attrValues p.bevelPackages)
-          );
-          withHoogle = true;
-          doBenchmark = true;
-          buildInputs = with pkgs; [
-            niv
-            zlib
-            cabal-install
-          ] ++ (with pre-commit-hooks;
-            [
-              hlint
-              hpack
-              nixpkgs-fmt
-              ormolu
-              cabal2nix
-            ]);
-          shellHook = self.checks.${system}.pre-commit.shellHook;
-        };
-        nixosModules.default = mkNixosModule { envname = "production"; };
-        nixosModuleFactories.default = mkNixosModule;
-        homeManagerModules.default = import ./nix/home-manager-module.nix { bevelReleasePackages = pkgs.bevelReleasePackages; };
-      });
+      };
+      devShells.${system}.default = pkgs.haskellPackages.shellFor {
+        name = "bevel-shell";
+        packages = (p:
+          (builtins.attrValues p.bevelPackages)
+        );
+        withHoogle = true;
+        doBenchmark = true;
+        buildInputs = with pkgs; [
+          niv
+          zlib
+          cabal-install
+        ] ++ (with pre-commit-hooks;
+          [
+            hlint
+            hpack
+            nixpkgs-fmt
+            ormolu
+            cabal2nix
+          ]);
+        shellHook = self.checks.${system}.pre-commit.shellHook;
+      };
+      nixosModules.${system}.default = mkNixosModule { envname = "production"; };
+      nixosModuleFactories.${system}.default = mkNixosModule;
+      homeManagerModules.${system}.default = import ./nix/home-manager-module.nix { bevelReleasePackages = pkgs.bevelReleasePackages; };
+    };
 }
