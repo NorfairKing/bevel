772ad97ee5f873e4ae994f27e1f1ca69bf160e42
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-05-20 18:01:39 +0200

    getting a bit stuck

--- bevel-select/src/main.rs
+++ bevel-select/src/main.rs
@@ -3,18 +3,22 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+use ordered_float::OrderedFloat;
+use ordered_map::OrderedMap;
 use std::{
     io,
-    time::{Duration, Instant},
+    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
 };
 use tui::{
     backend::{Backend, CrosstermBackend},
     layout::{Alignment, Constraint, Direction, Layout},
     style::{Color, Style},
     text::{Span, Spans},
-    widgets::Paragraph,
+    widgets::{List, ListItem, ListState, Paragraph},
     Frame, Terminal,
 };
+
+use sqlite::State;
 
 fn main() -> Result<(), io::Error> {
     // setup terminal
@@ -32,7 +36,7 @@ fn main() -> Result<(), io::Error> {
     )
     .unwrap();
 
-    let app = App::new(connection);
+    let app = App::new(&connection);
     let res = run_app(&mut terminal, app);
 
     // restore terminal
@@ -49,12 +53,22 @@ fn main() -> Result<(), io::Error> {
 
 fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
     // 1000 milliseconds is a second.
-    let frames_per_second = 4;
+    let frames_per_second = 30;
     let tick_rate = Duration::from_millis(1000 / frames_per_second);
+
     let mut last_tick = Instant::now();
     loop {
         terminal.draw(|f| ui(f, &mut app))?;
 
+        // If there are still more rows to load.
+        if app.loaded < app.total {
+            // As long as we still have time within this tick, load some more rows
+            while last_tick.elapsed() <= tick_rate {
+                app.load_rows(256);
+            }
+        }
+
+        // Wait for an event, or until the tick ends.
         let timeout = tick_rate
             .checked_sub(last_tick.elapsed())
             .unwrap_or_else(|| Duration::from_secs(0));
@@ -75,31 +89,99 @@ fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<(
 
 fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let chunks = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(100)].as_ref())
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(90), Constraint::Percentage(10)].as_ref())
         .split(f.size());
+
+    let items: Vec<ListItem> = app
+        .choices
+        .items
+        .descending_keys()
+        .take(20)
+        .map(|command| ListItem::new(command.text.clone()))
+        .collect();
+    let choices_list = List::new(items).highlight_symbol("> ");
+    f.render_stateful_widget(choices_list, chunks[0], &mut app.list_state);
+
+    // The counter on the bottom right
+    let loaded_colour = if app.loaded >= app.total {
+        Color::Green
+    } else {
+        Color::Red
+    };
     let loaded_text = vec![Spans::from(vec![
-        Span::styled(format!("{}", app.loaded), Style::default().fg(Color::Red)),
+        Span::styled(
+            format!("{}", app.loaded),
+            Style::default().fg(loaded_colour),
+        ),
         Span::styled(" / ", Style::default().fg(Color::Yellow)),
         Span::styled(format!("{}", app.total), Style::default().fg(Color::Green)),
     ])];
     let loaded_label = Paragraph::new(loaded_text).alignment(Alignment::Right);
-    f.render_widget(loaded_label, chunks[0]);
+    f.render_widget(loaded_label, chunks[1]);
 }
 
-struct App {
+struct App<'a> {
+    connection: &'a sqlite::Connection,
+    list_state: ListState,
+    choices: Choices,
     loaded: u64,
     total: u64,
 }
-const STARTING_COUNT_QUERY: &str = "SELECT COUNT(*) from command";
-impl App {
-    pub fn new(connection: sqlite::Connection) -> Self {
+impl<'a> App<'a> {
+    pub fn new(connection: &'a sqlite::Connection) -> Self {
+        const STARTING_COUNT_QUERY: &str = "SELECT COUNT(*) from command";
         let mut statement = connection.prepare(STARTING_COUNT_QUERY).unwrap();
         statement.next().unwrap();
         let total = statement.read::<i64, _>("COUNT(*)").unwrap();
         App {
+            connection: &connection,
+            list_state: ListState::default(),
+            choices: Choices::new(),
             loaded: 0,
             total: total as u64,
         }
+    }
+
+    pub fn load_rows(&mut self, rows: u64) {
+        let offset = self.loaded as i64;
+        let limit = rows as i64;
+        const ROW_LOADING_QUERY: &str = "SELECT workdir, begin FROM command LIMIT ? OFFSET ?";
+        let mut statement = self.connection.prepare(ROW_LOADING_QUERY).unwrap();
+        statement.bind((1, limit)).unwrap();
+        statement.bind((2, offset)).unwrap();
+
+        while let Ok(State::Row) = statement.next() {
+            self.loaded += 1;
+            let workdir = statement.read::<String, _>("workdir").unwrap();
+            let begin = statement.read::<i64, _>("begin").unwrap();
+            self.choices.add(workdir, begin);
+        }
+    }
+}
+
+struct Choices {
+    now: i64,
+    items: OrderedMap<String, f64, OrderedFloat<f64>>,
+}
+
+const NANOSECONDS_IN_A_DAY: f64 = 86400_000_000_000_f64;
+
+impl Choices {
+    pub fn new() -> Self {
+        Choices {
+            now: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_nanos() as i64,
+            items: OrderedMap::new(|s| OrderedFloat(*s)),
+        }
+    }
+
+    // Add a (workdir, begin) pair after computing its score
+    pub fn add(&mut self, workdir: String, begin: i64) {
+        let timediff = (self.now - begin) as f64;
+        let score = NANOSECONDS_IN_A_DAY / timediff;
+        self.items.insert(workdir, score);
     }
 }
