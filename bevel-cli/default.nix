f9124ef841cdaad7a0d86c846f25f0bc22d18004
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-05-22 16:33:33 +0200

    remove the old bevel tui

--- bevel-cli/default.nix
+++ bevel-cli/default.nix
@@ -1,13 +1,12 @@
 { mkDerivation, aeson-pretty, appendful-persistent, async
 , autodocodec, autodocodec-yaml, base, bevel-api
 , bevel-api-server-data, bevel-api-server-gen, bevel-client
-, bevel-client-data, bevel-client-data-gen, bevel-data, brick
-, bytestring, conduit, containers, cookie, criterion, cursor
-, cursor-brick, envparse, esqueleto, filelock, filepath
-, genvalidity, genvalidity-sydtest, genvalidity-text
-, genvalidity-time, hostname, http-client, http-client-tls, lib
-, monad-logger, mtl, optparse-applicative, path, path-io
-, persistent, persistent-pagination, persistent-sqlite, QuickCheck
+, bevel-client-data, bevel-client-data-gen, bevel-data, bytestring
+, conduit, containers, cookie, envparse, esqueleto, filelock
+, filepath, genvalidity-sydtest, genvalidity-text, genvalidity-time
+, hostname, http-client, http-client-tls, lib, monad-logger, mtl
+, optparse-applicative, path, path-io, persistent
+, persistent-pagination, persistent-sqlite, QuickCheck
 , servant-client, sydtest, sydtest-discover, text, time
 , typed-process, unix, unliftio, vector, vty, witherable, yaml
 }:
@@ -20,12 +19,11 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson-pretty appendful-persistent async autodocodec
     autodocodec-yaml base bevel-api bevel-api-server-data bevel-client
-    bevel-client-data bevel-data brick bytestring conduit containers
-    cookie cursor cursor-brick envparse esqueleto filelock filepath
-    hostname http-client http-client-tls monad-logger mtl
-    optparse-applicative path path-io persistent persistent-pagination
-    persistent-sqlite servant-client text time unix unliftio vector vty
-    witherable yaml
+    bevel-client-data bevel-data bytestring conduit containers cookie
+    envparse esqueleto filelock filepath hostname http-client
+    http-client-tls monad-logger mtl optparse-applicative path path-io
+    persistent persistent-pagination persistent-sqlite servant-client
+    text time unix unliftio vector vty witherable yaml
   ];
   executableHaskellDepends = [ base ];
   testHaskellDepends = [
@@ -36,11 +34,6 @@ mkDerivation {
     typed-process
   ];
   testToolDepends = [ sydtest-discover ];
-  benchmarkHaskellDepends = [
-    base bevel-client-data bevel-client-data-gen bevel-data conduit
-    criterion genvalidity monad-logger path path-io persistent
-    persistent-sqlite QuickCheck text time
-  ];
   homepage = "https://github.com/NorfairKing/bevel#readme";
   license = lib.licenses.unfree;
   hydraPlatforms = lib.platforms.none;
